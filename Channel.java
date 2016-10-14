6010340f4b16c193aa29648e13fb2cd6f52e81ae
Ezekiel Newren <ezekielnewren@gmail.com>
2016-10-14 09:03:07 -0600

    added field update methods

--- Channel.java
+++ Channel.java
@@ -1,21 +1,32 @@
 package com.github.ezekielnewren.net.multiplexer;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 class Channel implements Closeable {
 
 	final Multiplexer home;
 	final int channel;
 	final Object mutex;
-
+	final int recvBufferSize;
+	final int sendBufferSize;
+	
+	final ByteArrayCircularBuffer window;
+	
 	boolean localInputClosed = false;
 	boolean localOutputClosed = false;
 	boolean remoteOutputClosed = false;
 
+	int written = 0;
+	int processed = 0;
+	
 	Channel(Multiplexer inst, int channel, int recvBufferSize, int sendBufferSize) {
 		this.home = inst;
 		this.channel = channel;
 		mutex = home.mutex;
+		this.recvBufferSize = recvBufferSize;
+		this.sendBufferSize = sendBufferSize;
+		window = new ByteArrayCircularBuffer(recvBufferSize);
 	}
 
 	public boolean isConnectionClosed() {
@@ -29,6 +40,41 @@ class Channel implements Closeable {
 			return localInputClosed&&localOutputClosed&&remoteOutputClosed;
 		}
 	}
+	
+	void incWritten(int amount) {
+		synchronized(mutex) {
+			written += amount;
+		}
+	}
+	
+	void decWritten(int amount) {
+		synchronized(mutex) {
+			written -= amount;
+		}
+	}
+	
+	void incProcessed(int amount) {
+		synchronized(mutex) {
+			processed += amount;
+		}
+	}
+	
+	private int clearProcessed() {
+		synchronized(mutex) {
+			int x = processed;
+			processed = 0;
+			return x;
+		}
+	}
+	
+	void feed(byte[] b, int off, int len) throws IOException {
+		window.write(b, off, len);
+	}
+	
+	void writePacket(byte[] b, int off, int len) throws IOException {
+		home.writePacket(channel, clearProcessed(), b, off, len, Multiplexer.FLAG_NULL);
+	}
+	
 	
 	@Override
 	public void close() {
