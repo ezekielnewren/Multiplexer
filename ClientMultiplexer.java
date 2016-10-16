ce7e616ecba5c8256d131b4b7a4f889db0074937
Ezekiel Newren <ezekielnewren@gmail.com>
2016-10-16 09:44:55 -0600

    changed methods to return specifically a Stream or DatagramPacket Channel

--- ClientMultiplexer.java
+++ ClientMultiplexer.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 public interface ClientMultiplexer extends CoreMultiplexer {
 
-	public Channel connect(int channel, int recvBufferSize, long timeout) throws IOException;
-	public Channel connect(int channel, int bufferSize) throws IOException;
+	public Channel connectDatagramPacketChannel(int channel, int recvBufferSize, long timeout) throws IOException;
+	public Channel connectDatagramPacketChannel(int channel, int bufferSize) throws IOException;
+	
+	public Channel connectStreamChannel(int channel, int recvBufferSize, long timeout) throws IOException;
+	public Channel connectStreamChannel(int channel, int bufferSize) throws IOException;
 	
 }
