9e1f0a9ff1f2c706cd51987ec754b42742568019
Ezekiel Newren <ezekielnewren@gmail.com>
2016-10-15 18:40:55 -0600

    Updated receive send and feed methods

--- DatagramPacketChannel.java
+++ DatagramPacketChannel.java
@@ -16,27 +16,56 @@ public class DatagramPacketChannel extends Channel {
 			while(packetList.size()==0) {
 				try{mutex.wait();}catch(InterruptedException e){Thread.currentThread().interrupt();}
 			}
-			
+
 			int packLen = packetList.popInt();
 			int loRead = Math.min(packLen, p.getLength());
 			int theRest = packLen-loRead;
 			window.read(p.getData(), p.getOffset(), loRead);
-			window.getInputStream().skip(8);
 			window.skip(theRest);
 			
+			super.incProcessed(packLen);
 		}
 	}
 	
 	public void send(DatagramPacket p) throws IOException {
 		synchronized(mutex) {
 			super.writePacket(p.getData(), p.getOffset(), p.getLength());
+			super.incWritten(p.getLength());
 		}
+	}
+	
+	public int getSendBufferSize() {
+		return super.sendBufferSize;
+	}
+	
+	public int getReceiveBufferSize() {
+		return super.recvBufferSize;
 	}
 	
 	@Override
 	void feed(byte[] b, int off, int len) throws IOException {
-		super.feed(b, off, len);
-		packetList.push(len);
+		synchronized(mutex) {
+			super.feed(b, off, len);
+			packetList.push(len);
+			mutex.notifyAll();
+		}
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
